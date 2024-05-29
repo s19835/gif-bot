4d94c062accf6bc58632558b51f0b93a34964211
Maria Niranjan <130844959+s19835@users.noreply.github.com>
2024-05-29 11:48:34 +0530

    completed deployment script successfully

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,7 +8,7 @@ const command = [];
 
 if ('data' in commands && 'execute' in commands) {
     command.push(commands.data);
-    console.log(command);
+    //console.log(command);
 } else {
     console.log("[WARNING] command not found");
 }
@@ -16,3 +16,20 @@ if ('data' in commands && 'execute' in commands) {
 //using REST and Routes
 //prepare an instance for REST
 const rest = new REST().setToken(process.env.TOKEN);
+//console.log(rest);
+
+//deploy commands
+(async () => {
+    try {
+        console.log(`Started refreshing ${command.length} application commands.`);
+
+        const data = await rest.put(Routes.applicationGuildCommand(process.env.CLIENTID, process.env.SEVERID), {body: command});
+        //console.log(data);
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+//console.error("This is an error!");
